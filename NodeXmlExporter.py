5b323fd47e0385872e9059ed6d296a0b7c8ac0e9
Ben Becker <ben@beckersolutions.com>
2024-01-10 23:30:05 +0000

    work in progress

--- NodeXmlExporter.py
+++ NodeXmlExporter.py
@@ -6,8 +6,9 @@ import sys
 import progressbar
 
 from asyncua import Client
-
 from XmlExporter import XmlExporter
+
+from asyncua.crypto import security_policies
 
 
 class NodeXMLExporter:
@@ -27,15 +28,18 @@ class NodeXMLExporter:
         self.nodes.append(node)
         self.logger.debug("Add %s" % node)
         browse_progressbar.update(len(self.nodes))
-        # iterate over all referenced nodes (31), only hierarchical references (33)
+        # iterate over all referenced nodes (31),
+        # only hierarchical references (33)
         for child in await node.get_children(refs=33):
             if child not in self.nodes:
                 await self.iterater_over_child_nodes(child, browse_progressbar)
 
-    async def export_xml(self, namespaces=None, output_file="export.xml", export_values=False):
+    async def export_xml(self, namespaces=None, output_file="export.xml",
+                         export_values=False):
         if namespaces:
             self.logger.info("Export only NS %s" % namespaces)
-            nodes = [node for node in self.nodes if node.nodeid.NamespaceIndex in namespaces]
+            nodes = [node for node in self.nodes if node.nodeid.NamespaceIndex
+                     in namespaces]
         else:
             nodes = self.nodes
 
@@ -47,12 +51,12 @@ class NodeXMLExporter:
         await exp.write_xml(output_file)
         self.logger.info("Export finished")
 
-    async def import_nodes(self, server_url="opc.tcp://localhost:16664", username="", password=""):
-        from asyncua.crypto import security_policies
-        import types
-        from asyncua.ua.uaprotocol_hand import CryptographyNone
+    async def import_nodes(self, server_url="opc.tcp://localhost:16664",
+                           username="", password="", security=False):
+        from asyncua import ua
 
         self.client = Client(server_url)
+
         if username is not None:
             self.client.set_user(username)
 
@@ -68,7 +72,21 @@ class NodeXMLExporter:
         # def signature(self, data):
         #    return None
         # fixed_signature = types.MethodType(signature, CryptographyNone)
-        # self.client.security_policy.asymmetric_cryptography.signature = fixed_signature
+        # self.client.security_policy.asymmetric_cryptography.signature =
+        #    fixed_signature
+
+        mode = "SignAndEncrypt"
+        certificate = "cert.der"
+        private_key = "key.pem"
+        policy_class = security_policies.SecurityPolicyBasic256Sha256
+        mode_class = getattr(ua.MessageSecurityMode, mode)
+
+        if security:
+            await self.client.set_security(
+                policy=policy_class,
+                certificate=certificate,
+                private_key=private_key,
+                mode=mode_class)
 
         try:
             await self.client.connect()
@@ -84,7 +102,8 @@ class NodeXMLExporter:
             self.namespaces[await self.client.get_namespace_index(ns)] = ns
 
         root = self.client.get_root_node()
-        self.logger.info("Starting to collect nodes. This may take some time ...")
+        self.logger.info("Starting to collect nodes. "
+                         "This may take some time ...")
         await self.start_node_browse(root)
         self.logger.info("All nodes collected")
 
@@ -110,35 +129,51 @@ class NodeXMLExporter:
         for ns in typecounts_per_namespace:
             self.logger.info("NS%d (%s)" % (ns, self.namespaces[ns]))
             for type_info in typecounts_per_namespace[ns]:
-                self.logger.info("\t%s:\t%d" % (type_info, typecounts_per_namespace[ns][type_info]))
+                self.logger.info("\t%s:\t%d" % (
+                    type_info, typecounts_per_namespace[ns][type_info]
+                ))
         self.logger.info("\tTOTAL in namespace: %d" % len(self.nodes))
 
 
 async def main():
     parser = argparse.ArgumentParser(
         description="Export Node XML from OPC UA server")
-    parser.add_argument('serverUrl', help='Complete URL of the OPC UA server', default="opc.tcp://localhost:16664")
+    parser.add_argument('serverUrl', help='Complete URL of the OPC UA server',
+                        default="opc.tcp://localhost:16664")
     parser.add_argument('-n', '--namespace',
                         metavar='<namespace>',
                         dest="namespaces",
                         action="append",
                         type=int,
-                        help='Export only the given namespace indexes. Multiple NS indexes can be specified. If not specified, export all nodes.')
+                        help='Export only the given namespace indexes. '
+                             'Multiple NS indexes can be specified. '
+                             'If not specified, export all nodes.')
     parser.add_argument('outputFile',  default="nodes_output.xml",
                         help='Save exported nodes in specified XML file')
-    parser.add_argument('-u', '--username', default="", metavar='<username>', dest="username", help="Username to login on server")
-    parser.add_argument('-p', '--password', default="", metavar='<password>', dest="password", help="Password to login on server")
-    parser.add_argument('-v', '--values', default=False, metavar='<values>', dest="export_values", help="Export node values to nodeset")
+    parser.add_argument('-u', '--username', default="", metavar='<username>',
+                        dest="username", help="Username to login on server")
+    parser.add_argument('-p', '--password', default="", metavar='<password>',
+                        dest="password", help="Password to login on server")
+    parser.add_argument('-s', '--security', default=False,
+                        metavar='<security>',
+                        dest="security", help="Enable default security")
+    parser.add_argument('-v', '--values', default=False, metavar='<values>',
+                        dest="export_values", help='Export node values '
+                                                   'to nodeset')
     args = parser.parse_args()
 
     exporter = NodeXMLExporter()
-    await exporter.import_nodes(server_url=args.serverUrl, username=args.username, password=args.password)
+    await exporter.import_nodes(server_url=args.serverUrl,
+                                username=args.username,
+                                password=args.password,
+                                security=args.security)
     await exporter.statistics()
-    await exporter.export_xml(args.namespaces, args.outputFile, args.export_values)
+    await exporter.export_xml(args.namespaces, args.outputFile,
+                              args.export_values)
 
     await exporter.client.disconnect()
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.WARN, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logging.basicConfig(level=logging.WARN, format="%(asctime)s - "
+                        "%(name)s - %(levelname)s - %(message)s")
     asyncio.run(main())
